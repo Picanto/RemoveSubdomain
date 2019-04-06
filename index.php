61a0e84d69c109e859699c11e3fced0186a6273f
picanto <compot2004@yandex.ru>
2019-04-06 20:14:28 +0300

    debug code deleted

--- index.php
+++ index.php
@@ -5,9 +5,6 @@ function RemoveSubdomain($url)
     $Sheme = parse_url($url, PHP_URL_SCHEME);
     $Result = str_replace($Sheme . '://', '', $url);
 
-    //preg_match('/(?:http[s]*\:\/\/)*(.*?)\.(?=[^\/]*\..{2,5})/i', $Result, $match);
-    //echo 'match = ' . $match[0] . '<br>';
-
     if (stristr($url, 'www')) {
         $Arr = explode('.', $url);
         if ($Arr[1] !== 'tnt4') {
@@ -21,14 +18,5 @@ function RemoveSubdomain($url)
         }
         $Result = $Sheme . '://' . implode('.', $Arr);
     }
-    return $url . '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . $Result;
+    return $Result;
 }
-
-echo RemoveSubdomain('https://tnt4.ru/') . '<br>';
-echo RemoveSubdomain('https://ulitsa2.tnt4.ru/') . '<br>';
-echo RemoveSubdomain('https://www.tnt4.ru/') . '<br>';
-echo RemoveSubdomain('https://www.ulitsa2.tnt4.ru/') . '<br>';
-echo RemoveSubdomain('https://ulitsa2.tnt4.ru/') . '<br>';
-echo RemoveSubdomain('https://casting.tnt4.ru/') . '<br>';
-echo RemoveSubdomain('https://chistoporjat.tnt4.ru/') . '<br>';
-echo RemoveSubdomain('https://dip.tnt4.ru/') . '<br>';
